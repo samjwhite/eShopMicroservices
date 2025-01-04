76af568445bd0ea573dcb1bbde2505f4463ecded
Serge Riazanov <65174541+samjwhite@users.noreply.github.com>
2025-01-04 17:24:38 -0500

    add BuildingBlocks class library to manage CQRS abstractions and better package version management.

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,11 @@
+//Vertical Slice Architecture
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the container.
+
+var app = builder.Build();
+
+//Configure the HTTP request pipeline.
+
+app.Run();
