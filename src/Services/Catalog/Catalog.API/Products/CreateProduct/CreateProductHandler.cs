691fe8a90d8a4ffb0a6b4aabb64077acb10fb750
Serge Riazanov <65174541+samjwhite@users.noreply.github.com>
2025-01-05 16:24:26 -0500

    Handle Product Command Add Post method, use Carter and Mapster packages

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,32 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, string Description, List<string> Category, string ImageFile, decimal Price)
-    :IRequest<CreateProductResult>;
+    :ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        //Business logic to create a product
-        throw new NotImplementedException();
+        //Create Product entity from command object
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Description = command.Description,
+            Category = command.Category,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //Save product entity to database
+
+
+        //return CreateProductResult result
+
+        return new CreateProductResult(Guid.NewGuid());
     }
 }
 
