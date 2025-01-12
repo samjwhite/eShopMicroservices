e8b859e98ce72b7ae8d761330e38463300d58a28
Serge Riazanov <65174541+samjwhite@users.noreply.github.com>
2025-01-12 14:59:08 -0500

    add get all products vertical slice

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Catalog.API.Products.GetProducts;
+
+//public record GetProductsRequest();
+public record GetProductsResponse(IEnumerable<Product> Products);
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var response = result.Adapt<GetProductsResponse>();
+            return Results.Ok(response);
+        })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .WithSummary("GetProducts")
+            .WithDescription("Get all products");
+    }
+}
+
