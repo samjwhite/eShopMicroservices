03b1f859ea43275d415211fe31281eca9d9ae433
Serge Riazanov <65174541+samjwhite@users.noreply.github.com>
2025-01-12 14:59:57 -0500

    add actual session for create product handler

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,12 +1,10 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, string Description, List<string> Category, string ImageFile, decimal Price)
     :ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
-internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -22,11 +20,12 @@ internal class CreateProductCommandHandler : ICommandHandler<CreateProductComman
         };
 
         //Save product entity to database
-
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
         //return CreateProductResult result
 
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }
 
