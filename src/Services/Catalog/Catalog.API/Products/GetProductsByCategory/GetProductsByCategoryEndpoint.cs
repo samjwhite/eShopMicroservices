b7c46543c2a1205ce3b9bd003fb2a40db35aba3b
Serge Riazanov <65174541+samjwhite@users.noreply.github.com>
2025-01-13 19:45:57 -0500

    adding getProductsByCategory handler/endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryEndpoint.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Catalog.API.Products.GetProductsByCategory;
+
+//public record GetProductsByCategoryRequest();
+
+public record GetProductsByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductsByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (ISender sender, string category) =>
+        {
+            var result = await sender.Send(new GetProductsByCategoryQuery(category));
+            var response = result.Adapt<GetProductsByCategoryResponse>();
+            return Results.Ok(response);
+        })
+            .WithName("GetProductsByCategory")
+            .Produces<GetProductsByCategoryResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("GetProductsByCategory")
+            .WithDescription("Get products by category");
+    }
+}
