a00741bbbedc6c190f628a2cf787f5f5f039d6a9
Serge Riazanov <65174541+samjwhite@users.noreply.github.com>
2025-04-13 19:35:51 -0400

    add CachedBasketRepository, using Redis. register decorated CachedBasketRepository and StackExchangeRedis with Scrutor, update launchSettings and doker-compose files.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -17,7 +17,15 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
